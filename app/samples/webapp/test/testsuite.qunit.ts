632cbefae72cba542fc4724568db23392b542df3
D048415 <dominik.heim@sap.com>
2025-07-18 16:02:44 +0200

    adjust naming

--- app/samples/webapp/test/testsuite.qunit.ts
+++ app/samples/webapp/test/testsuite.qunit.ts
@@ -1,5 +1,5 @@
 export default {
-    name: "QUnit test suite for the UI5 Application: uimodule",
+    name: "QUnit test suite for the UI5 Application: com.sap.cap.fe.ts.sample",
     defaults: {
         page: "ui5://test-resources/com/sap/cap/fe/ts/sample/Test.qunit.html?testsuite={suite}&test={name}",
         qunit: {
