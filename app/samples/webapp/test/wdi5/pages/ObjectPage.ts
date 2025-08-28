730513b15e631df08de81a2230d4af4d1b26ceff
D048415 <dominik.heim@sap.com>
2025-08-28 09:31:23 +0200

    wdi5 change type to promise

--- app/samples/webapp/test/wdi5/pages/ObjectPage.ts
+++ app/samples/webapp/test/wdi5/pages/ObjectPage.ts
@@ -1,5 +1,12 @@
 import type List from "sap/m/List";
 import {wdi5Selector} from "wdio-ui5-service";
+import type { WDI5Control } from "wdio-ui5-service/cjs/lib/wdi5-control";
+
+type MapFunctionToPromise<T> = T extends (...args: infer A) => infer R ? (...args: A) => Promise<R> : T;
+
+type Promised<T> = WDI5Control & {
+    [K in keyof T]: MapFunctionToPromise<T[K]>;
+}
 
 // open: typed UI5 controls retrieved from the browser-scope (see https://github.com/ui5-community/wdi5/issues/509)
 
@@ -12,8 +19,7 @@ class ObjectPage {
                 id: RegExp("CommentsList")
             }
         };
-        const commentsList = await browser.asControl<List>(commentsListSelector);
-        // eslint-disable-next-line @typescript-eslint/await-thenable
+        const commentsList = await browser.asControl<List>(commentsListSelector) as unknown as Promised<List>;
         const commentsListItems = await commentsList.getItems();
         await expect(commentsListItems.length).toEqual(numberOfEntries);
     }
