81bb06fb9c9d1507b6b12b814812b33c2fd9fdb5
D048415 <dominik.heim@sap.com>
2026-01-08 17:33:35 +0100

    delete fiori tools eslint plugin ignores override

--- app/samples/eslint.config.js
+++ app/samples/eslint.config.js
@@ -1,5 +1,4 @@
 const fioriTools  = require('@sap-ux/eslint-plugin-fiori-tools');
-const tsParser = require('@typescript-eslint/parser');
 
 module.exports = [
     {
@@ -12,17 +11,5 @@ module.exports = [
             'linebreak-style': 0
         }
     },
-    ...fioriTools.configs.recommended,
-    {
-        // OVERRIDE: Re-include localService TS files
-        files: ['webapp/localService/**/*.ts'],
-        languageOptions: {
-            parser: tsParser,
-            sourceType: 'module',
-            ecmaVersion: 'latest'
-        },
-        rules: {
-            'linebreak-style': 0
-        }
-    },
+    ...fioriTools.configs.recommended
 ];
